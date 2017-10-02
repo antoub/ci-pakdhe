8572231c9ca5cef301d1e6256c0952b8d44d60f1
antoub <antoub@gmail.com>
2017-10-02 09:31:18 +0700

    users

--- application/modules/acl/controllers/Users.php
+++ application/modules/acl/controllers/Users.php
@@ -5,59 +5,35 @@ class Users extends MY_Admin {
 	function __construct() {
 		parent::__construct();
 		$this->data['tpl']='single';
-		$this->data['subicon']='fa fa-users';
+		$this->data['icon']='fa fa-cogs';
+		$this->data['subicon']='fa fa-university';
 		$this->data['title']='Users';
 		$this->data['table_name'] = 'users';
-		$this->data['content']='';				
+		$this->data['content']='';
 		$this->data['css']=css_asset('bootstrap-table.min.css','bootstrap-table');
+		$this->data['css'].=css_asset('bootstrap3-wysihtml5.min.css','bootstrap-wysihtml5');
 		$this->data['js']=js_asset('bootstrap-table.min.js','bootstrap-table');
+		$this->data['js'].=js_asset('bootstrap3-wysihtml5.all.min.js','bootstrap-wysihtml5');
 		$this->data['auth_meta'] = $this->meta('acl/users/',true);
 	}
 
 	function index(){
-		// $meta = $this->meta('acl/groups/',true);
 		$this->load->library('form_validation');
 		$mydata['auth_meta'] = $this->meta('acl/users/',true);
-		//print_r($mydata);die;
 		$mydata['tbl_icon']=$this->data['subicon'];
 		$mydata['tbl_title']=$this->data['title'];
 		$mydata['tbl']='mytabel';
 
 		$mydata['groups'] = $this->db->get('groups')->result();
-		$this->db->order_by('parent_name,name','asc');		
+		$this->db->order_by('parent_id,name','asc');		
 		$mydata['orgs'] = $this->db->get('orgs')->result_array();
 		
 		//for org,bid,sie 
 		$mydata['tree_org']=$this->buildtree($mydata['orgs']);
-		
-		/*
-		$arr_tree = $this->the_org_child_tree($_SESSION['user_org']);
-		
-		if(count($arr_tree)){
-			$mydata['tree_org']=$arr_tree;
-		}else{
-			$mydata['tree_org']=array($_SESSION['user_org']);
-		}
-		//print_r($_SESSION['user_org']);
-		*/
-		//print_r($mydata['tree_org']);
 		$this->data['content']=$this->load->view('grid_users',$mydata,true);
 		$this->display();
-		
 	}
 
-	function buildtree($src_arr, $parent_id = 0, $tree = array()) {
-    foreach($src_arr as $idx => $row){
-			if($row['parent_id'] == $parent_id){
-				foreach($row as $k => $v)
-					$tree[$row['id']][$k] = $v;
-				unset($src_arr[$idx]);
-				$tree[$row['id']]['children'] = $this->buildtree($src_arr, $row['id']);
-			}
-    }
-    ksort($tree);
-    return $tree;
-	}
 
 	function get_json(){
 		$ret = array(
@@ -71,48 +47,24 @@ class Users extends MY_Admin {
 		$search=(isset($_GET['search'])) ? $_GET['search'] : '';
 		$sort = (isset($_GET['sort'])) ? $_GET['sort'] : 'email,username';
 		$order = (isset($_GET['order'])) ? $_GET['order'] : 'asc';
-		/*
-		$SQL_BASE="SELECT * FROM (
-		SELECT 
-			a2.*,  
-			GROUP_CONCAT(orgs.name SEPARATOR ', ') AS orgs, 
-			GROUP_CONCAT(orgs.id SEPARATOR ', ') as orgs_id 
-		FROM (
-			SELECT 
+		$SQL_BASE = "
+			select 
 			users.id,
 			users.username,
 			users.email, 
 			users.first_name,
-			users.last_name,  
-			GROUP_CONCAT(groups.name SEPARATOR ', ') AS groups,
-			GROUP_CONCAT(groups.id SEPARATOR ', ') as groups_id 
-			FROM users
-			LEFT JOIN users_groups ON users_groups.user_id = users.id
-			LEFT JOIN groups ON users_groups.group_id = groups.id
-			GROUP BY users.id) AS a2
-			LEFT JOIN users_orgs ON users_orgs.user_id = a2.id
-			LEFT JOIN orgs ON orgs.id = users_orgs.org_id
-			GROUP BY a2.id) AS a1 ";
-		*/
-		$SQL_BASE = "
-select 
-users.id,
-users.username,
-users.email, 
-users.first_name,
-users.last_name,
-users.company,
-users.phone,
-users_groups.group_id,
-groups.name as groups,
-users_orgs.org_id,
-orgs.name as orgs
-from 
-((((users left join users_groups on users_groups.user_id=users.id)
-left join groups on groups.id=users_groups.group_id)
-left join users_orgs on users_orgs.user_id=users.id)
-left join orgs on orgs.id=users_orgs.org_id)
-
+			users.last_name,
+			users.company,
+			users.phone,
+			users_groups.group_id,
+			groups.name as groups,
+			users_orgs.org_id,
+			orgs.name as orgs
+			from 
+			((((users left join users_groups on users_groups.user_id=users.id)
+			left join groups on groups.id=users_groups.group_id)
+			left join users_orgs on users_orgs.user_id=users.id)
+			left join orgs on orgs.id=users_orgs.org_id)
 		";
 		
 		if($search<>''){
@@ -123,9 +75,7 @@ left join orgs on orgs.id=users_orgs.org_id)
 			$SQL_BASE.='lower(first_name) like "%'.strtolower($search).'%" OR ';
 			$SQL_BASE.='lower(last_name) like "%'.strtolower($search).'%" OR ';
 			$SQL_BASE.='lower(groups.name) like "%'.strtolower($search).'%" OR ';
-			$SQL_BASE.='lower(orgs.name) like "%'.strtolower($search).'%" OR ';
-			$SQL_BASE.='lower(bid.name) like "%'.strtolower($search).'%" OR ';
-			$SQL_BASE.='lower(sie.name) like "%'.strtolower($search).'%" ';
+			$SQL_BASE.='lower(orgs.name) like "%'.strtolower($search).'%" ';
 			$ls_data=$this->db->query($SQL_BASE)->result_array();
 
 			$ret['total'] = count($ls_data);
@@ -167,8 +117,6 @@ left join orgs on orgs.id=users_orgs.org_id)
   		$this->form_validation->set_rules('password','Password','required');
   		$this->form_validation->set_rules('groups','Groups','required|integer');
   		$this->form_validation->set_rules('orgs','Orgs','required|integer');
-  		$this->form_validation->set_rules('bidang','Bidang','required|integer');
-  		$this->form_validation->set_rules('seksi','Seksi','required|integer');
 		
 		if($this->form_validation->run()===FALSE){
 			$ret=array(
@@ -200,8 +148,6 @@ left join orgs on orgs.id=users_orgs.org_id)
 
 				$data['user_id']=$user_id;
 				$data['org_id']=$org_id;
-				$data['bidang_id']=$bidang_id;
-				$data['seksi_id']=$seksi_id;
 				$this->db->insert('users_orgs', $data); 
 
 				$ret=array(
@@ -242,7 +188,6 @@ left join orgs on orgs.id=users_orgs.org_id)
   	$this->form_validation->set_rules('email','Email','trim|required|valid_email');
   	$this->form_validation->set_rules('groups[]','Groups','required|integer');
   	$this->form_validation->set_rules('id','User ID','trim|integer|required');
-  		// $this->form_validation->set_rules('orgs[]','Orgs','required|integer');
 
   		if($this->form_validation->run()===FALSE){
 			$ret=array(
@@ -283,8 +228,6 @@ left join orgs on orgs.id=users_orgs.org_id)
 
 						$data['user_id']=$id;
 						$data['org_id']=$org_id;
-						$data['bidang_id']=$bidang_id;
-						$data['seksi_id']=$seksi_id;
 						$this->db->insert('users_orgs', $data); 
 						
     		}
