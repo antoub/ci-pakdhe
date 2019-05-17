d12ed6bc746da75808f996df7eccdc9d6ed0d20e
Pakdhe Febri <antoub@gmail.com>
2019-05-17 14:56:28 +0700

    auto url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://127.0.0.1/ci-pakdhe/';
+//$config['base_url'] = 'http://127.0.0.1/ci-pakdhe/';
+$root=(isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'];
+$root.= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+$config['base_url'] = $root;
+
 
 /*
 |--------------------------------------------------------------------------
