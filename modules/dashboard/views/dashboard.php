2f187e2f7178a03f3aedb3a1ce60c7cd5af086ff
Pakdhe Febri <antoub@gmail.com>
2019-08-10 11:01:37 +0700

    change dashboard with logo app etc

--- modules/dashboard/views/dashboard.php
+++ modules/dashboard/views/dashboard.php
@@ -3,38 +3,46 @@
 		Dashboard
 		<small>Control panel</small>
 		<span class="pull-right">
-		<?php echo modules::run('acl/widget/group_org_user');?>					
+			<?php echo modules::run('acl/widget/group_org_user');?>					
 		</span>
 	</h1>
 </section>
-		
+
 <section class="content">
 	<div class="row">
 		<div class="col-md-6">
 			<?php include_once('form_profile.php');?>
 		</div>
-		<div class="col-md-3">
-				<?php echo modules::run('acl/widget_acl/users_box');?>			
-				<?php echo modules::run('acl/widget_acl/orgs_box');?>
-				<?php echo modules::run('pddk/widget_pddk/jml_pddk_box','Fakir Uzur');?>
-				<?php echo modules::run('pddk/widget_pddk/jml_pddk_box','Yatim');?>
-		</div>
-		<div class="col-md-3">
-				<?php echo modules::run('acl/widget_acl/groups_box');?>
-				<?php echo modules::run('pddk/widget_pddk/jml_box');?>
-				<?php echo modules::run('pddk/widget_pddk/jml_pddk_box','Piatu');?>
+		<div class="col-md-6">
+			<div class="panel">
+				<div class="panel-body" style="border:1px solid #AAA;">
+					<div class="media">
+						<div class="media-left media-top">
+							<img class="media-object" src="<?=base_url('assets/image/'.$this->MYCFG['logo']);?>" alt="<?=$this->MYCFG['logo'];?>" width="120px"/>
+						</div>
+						<div class="media-body">
+							<h3 class="media-heading"><?=$this->MYCFG['name_short'].$this->MYCFG['name_short2'].' Ver.'.$this->MYCFG['version'];?></h3>
+							<h4 class="media-heading"><?=$this->MYCFG['name_long'];?></h4>
+							<h5 class="media-heading"><?=$this->MYCFG['org_name'];?></h5>
+							<h5 class="media-heading"><?=$this->MYCFG['city_name'];?></h5>
+							<h5 class="media-heading"><?=$this->MYCFG['province_name'];?></h5>
+							<?php //print_r($this->MYCFG);?>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 	
 </section>
 
 <script>
-$(document).ready(function(){
-	
-	$('.tgl').each(function(){
-		var tgl=$(this).html();
-		$(this).html(moment($(this).html()).format("dddd, DD-MMM-YYYY HH:mm"));
+	$(document).ready(function(){
+		
+		$('.tgl').each(function(){
+			var tgl=$(this).html();
+			$(this).html(moment($(this).html()).format("dddd, DD-MMM-YYYY HH:mm"));
+		});
+		
 	});
-	
-});
 </script>
